402f75642e2c722152205497075ba80d1a585a52
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-29 23:23:43 -0300

    user get

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	_ "github.com/PedroDrago/DogsAPI/internal/models"
 	_ "github.com/lib/pq"
 )
 
--- internal/models/user.go
+++ internal/models/user.go
@@ -3,6 +3,8 @@ package models
 import (
 	"database/sql"
 	"time"
+
+	_ "github.com/lib/pq"
 )
 
 type UserModel struct {
@@ -28,7 +30,7 @@ func (model UserModel) Insert(user *User) error {
 	INSERT INTO users(name, username, email, birth_year, address, phone) 
 	VALUES ($1, $2, $3, $4, $5, $6) 
 	RETURNING id, created_at, updated_at;
-	`
+	` // TODO: I guess I'll need to Join here to get the Dogs array
 	args := []any{user.Name, user.Username, user.Email, user.BirthYear, user.Address, user.Phone}
 	err := model.DB.QueryRow(query, args...).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
@@ -39,6 +41,16 @@ func (model UserModel) Insert(user *User) error {
 
 func (model UserModel) Get(id int64) (User, error) {
 	usr := User{}
+	query := `
+	SELECT id, name, username, email, birth_year, address, phone, admin, created_at, updated_at 
+	FROM users
+	WHERE id = $1;
+	`
+	args := []any{&usr.ID, &usr.Name, &usr.Username, &usr.Email, &usr.BirthYear, &usr.Address, &usr.Phone, &usr.Admin, &usr.CreatedAt, &usr.UpdatedAt}
+	err := model.DB.QueryRow(query, id).Scan(args...)
+	if err != nil {
+		return usr, err
+	}
 	return usr, nil
 }
 
