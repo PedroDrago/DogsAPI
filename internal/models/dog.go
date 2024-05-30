1a85ecd1fd09f48bc9f8ac84903d2f6b05c48e43
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-30 02:21:21 -0300

    setup Dog CRUD boilerplate

--- internal/models/dog.go
+++ internal/models/dog.go
@@ -10,14 +10,36 @@ type DogModel struct {
 }
 
 type Dog struct {
-	ID           int64
-	Name         string
-	BirthYear    int32
-	Breed        string
-	Sex          string
-	SpecialNeeds []string
-	Neutered     bool
-	CreatedAt    time.Time
-	UpdatedAt    time.Time
-	Tutor        User
+	ID           int64     `json:"id"`
+	Name         string    `json:"name"`
+	BirthYear    int32     `json:"birth_year,omitempty"`
+	Breed        string    `json:"breed,omitempty"`
+	Sex          string    `json:"sex,omitempty"`
+	SpecialNeeds []string  `json:"special_needs,omitempty"`
+	Neutered     bool      `json:"neutered,omitempty"`
+	CreatedAt    time.Time `json:"-"`
+	UpdatedAt    time.Time `json:"-"`
+	Tutor        User      `json:"tutor"`
+}
+
+func (model DogModel) Insert(dog *Dog) error {
+	return nil
+}
+
+func (model DogModel) Get(id int64) (Dog, error) {
+	dog := Dog{}
+	return dog, nil
+}
+
+func (model DogModel) Update(user *User) error {
+	return nil
+}
+
+func (model DogModel) Delete(id int64) error {
+	return nil
+}
+
+func (model DogModel) Index(id int64) ([]Dog, error) {
+	dog := []Dog{}
+	return dog, nil
 }
