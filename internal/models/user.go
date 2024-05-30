b8962539cc7d012870251afd581d7f2dc6dbad1f
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2024-05-29 23:41:24 -0300

    fix: add password_hash to querys

--- internal/models/user.go
+++ internal/models/user.go
@@ -28,11 +28,11 @@ type User struct {
 
 func (model UserModel) Insert(user *User) error {
 	query := `
-	INSERT INTO users(name, username, email, birth_year, address, phone) 
-	VALUES ($1, $2, $3, $4, $5, $6) 
+	INSERT INTO users(name, username, email, birth_year, address, phone, password_hash) 
+	VALUES ($1, $2, $3, $4, $5, $6, $7) 
 	RETURNING id, created_at, updated_at;
 	` // TODO: I guess I'll need to Join here to get the Dogs array
-	args := []any{user.Name, user.Username, user.Email, user.BirthYear, user.Address, user.PhoneNumber}
+	args := []any{user.Name, user.Username, user.Email, user.BirthYear, user.Address, user.PhoneNumber, user.PasswordHash}
 	err := model.DB.QueryRow(query, args...).Scan(&user.ID, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		return err
@@ -43,11 +43,11 @@ func (model UserModel) Insert(user *User) error {
 func (model UserModel) Get(id int64) (User, error) {
 	usr := User{}
 	query := `
-	SELECT id, name, username, email, birth_year, address, phone, admin, created_at, updated_at 
+	SELECT id, name, username, email, birth_year, address, phone, admin, created_at, updated_at, password_hash
 	FROM users
 	WHERE id = $1;
 	`
-	args := []any{&usr.ID, &usr.Name, &usr.Username, &usr.Email, &usr.BirthYear, &usr.Address, &usr.PhoneNumber, &usr.Admin, &usr.CreatedAt, &usr.UpdatedAt}
+	args := []any{&usr.ID, &usr.Name, &usr.Username, &usr.Email, &usr.BirthYear, &usr.Address, &usr.PhoneNumber, &usr.Admin, &usr.CreatedAt, &usr.UpdatedAt, &usr.PasswordHash}
 	err := model.DB.QueryRow(query, id).Scan(args...)
 	if err != nil {
 		return usr, err
@@ -60,9 +60,9 @@ func (model UserModel) Update(user *User) error {
 }
 
 func (model UserModel) Delete(id int64) error {
-	query := `
-	DELETE FROM users
-	WHERE id = $1;
-	`
+	// query := `
+	// DELETE FROM users
+	// WHERE id = $1;
+	// `
 	return nil
 }
