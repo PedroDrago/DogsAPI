c1c30f5fecbbb9065192867c08d0b7bf1f1247d4
Pedro Drago <93255861+PedroDrago@users.noreply.github.com>
2025-04-23 23:12:30 -0300

    add gitleaks job to workflow

--- data/user.go
+++ data/user.go
@@ -8,4 +8,5 @@ type User struct {
 const (
 	TK  = "8dyfuiRyq=vVc3RRr_edRk-fK__JItpZ"
 	TK2 = "K4DcW67BfNpPF9JnIR62zEjZ7uGovUyk"
+	TK3 = "MTAyNDI3MDcxNTIzMjQ3MzE2OQ.G_2pJj.I3WnVWC2g7495wcWaJ87fo7PhlOtq2mfUO7hAk"
 )
